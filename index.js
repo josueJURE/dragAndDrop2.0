9b3d640ff6a6529a2bbb41d39314c8a44d717857
josueJURE <josue.jure@gmail.com>
2022-02-17 11:42:22 +0000

    use for loop and eventListeners

--- index.js
+++ index.js
@@ -1,12 +1,35 @@
 const empties = document.querySelectorAll(".empty");
 const image = document.querySelector(".fill")
 
-console.log(image)
+
 
 image.addEventListener("dragstart", function(e) {
   e.target.classList.add("dragImage");
+  console.log(e.target)
 })
 
 image.addEventListener("dragend", function(e) {
   e.target.classList.remove("dragImage");
 })
+
+
+
+for(var i = 0; i < empties.length; i++) {
+  var empty = empties[i];
+  empty.addEventListener("dragover", function(e) {
+    e.preventDefault();
+    console.log("draggedover")
+  })
+  empty.addEventListener("dragenter", function(e) {
+    e.preventDefault();
+    empty.classList.add("pink")
+    console.log("entered")
+  })
+  empty.addEventListener("dragleave", function() {
+    empty.classList.remove("pink")
+  })
+  empty.addEventListener("drop", function(e) {
+    empty.append(image)
+
+  })
+}
